5d39492af23c90c1164fcaeabc92e2b6e217bbbd
Geotale <72356786+Geotale@users.noreply.github.com>
2022-03-24 17:42:56 -0500

    Use spaces instead of tabs 2

--- render.js
+++ render.js
@@ -12,20 +12,20 @@ document.body.appendChild(renderCanvas);
 const penClear = () => penCtx.clearRect(0, 0, 480, 360);
 
 function drawPen(x1, y1, x2, y2, color, alpha, size){
-	if(alpha){
-		const l = color[2] * (1 - color[1] / 2);
-		penCtx.strokeStyle = `hsl(${3.6 * color[0]},${100 * (color[2] - l) / Math.min(l, 1 - l) || 0}%,${100 * l}%)`;
-		penCtx.globalAlpha = alpha;
-		penCtx.lineWidth = size;
-		penCtx.beginPath();
-		penCtx.moveTo(x1 + 240, 180 - y1);
-		penCtx.lineTo(x2 + 240, 180 - y2);
-		penCtx.stroke();
-	}
+  if(alpha){
+    const l = color[2] * (1 - color[1] / 2);
+    penCtx.strokeStyle = `hsl(${3.6 * color[0]},${100 * (color[2] - l) / Math.min(l, 1 - l) || 0}%,${100 * l}%)`;
+    penCtx.globalAlpha = alpha;
+    penCtx.lineWidth = size;
+    penCtx.beginPath();
+    penCtx.moveTo(x1 + 240, 180 - y1);
+    penCtx.lineTo(x2 + 240, 180 - y2);
+    penCtx.stroke();
+  }
 }
 
 function draw(){
-	renderCtx.fillStyle = "#fff";
-	renderCtx.fillRect(0, 0, 480, 360);
-	renderCtx.drawImage(penCanvas, 0, 0);
+  renderCtx.fillStyle = "#fff";
+  renderCtx.fillRect(0, 0, 480, 360);
+  renderCtx.drawImage(penCanvas, 0, 0);
 }
